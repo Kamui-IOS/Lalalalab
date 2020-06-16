6fd2e1f8c0145d3c0ee6dbff7883e31246bbe8a4
namhv@mobgame.vn <namhv@mobgame.vn>
2020-06-16 15:09:57 +0700

    first commit

---
+++ Lalalalab/Lalalalab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lalalalab
+//
+//  Created by Nam Hoang Van on 6/16/20.
+//  Copyright © 2020 Nam Hoang Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
